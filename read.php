049eeb20726b5ddc8f74e582159653d50bd850fc
Daiki Matoba <telnetstat@gmail.com>
2016-05-29 09:43:42 +0900

    Handle error with deleted video

--- read.php
+++ read.php
@@ -2,36 +2,38 @@
 if(!isset($_GET['v']))header("Location: ./");
 if(empty($_GET['v']))header("Location: ./");
 
-$ERROR = false;
-$MSG = "";
+$error = [];
 if(preg_match("/sm\d+/",$_GET['v'],$match)){
-	$v = $match[0];
-	require_once 'nicovideo.php';
+  $v = $match[0];
+  require_once 'nicovideo.php';
 
-	$nv = new Nicovideo();
-	$comment_data = $nv->get_comment($v);
-	if(!$comment_data){
-		$MSG = "動画が見つかりませんでした。。。";
-    $ERROR_TITLE = "動画が見つかりませんでした。。。";
-		$ERROR = true;
-	}
-	$info = simplexml_load_string(file_get_contents("http://ext.nicovideo.jp/api/getthumbinfo/{$v}"));
+  $nv = new Nicovideo();
+  $comment_data = $nv->get_comment($v);
+  if(!$comment_data){
+    $error['message'] = "動画が見つかりませんでした。。。";
+    $error['title'] = "動画が見つかりませんでした。。。";
+  }
+  $info = simplexml_load_string(file_get_contents("http://ext.nicovideo.jp/api/getthumbinfo/{$v}"));
+
+  if ($info->error) {
+    $error['title'] = "削除済み動画。。。";
+    $error['message'] = "動画は削除されました。。。";
+  }
 
 }else{
-	$ERROR = true;
-  $ERROR_TITLE = "URLが変です。。。";
-	$MSG = "URLが変です。。。<br>あと公式動画とかは対応してないです。。。";
+  $error['title'] = "URLが変です。。。";
+  $error['message'] = "URLが変です。。。<br>あと公式動画とかは対応してないです。。。";
 }
 
 function time2str($time){
-	if(preg_match("/^\d+$/",$time)){
-		$time = intval($time);
-	}else{
-		$time = strtotime($time);
-	}
-	$youbi = ['日','月','火','水','木','金','土'];
-	$date_str = date("Y/m/d",$time)."(".$youbi[intval(date("w",$time))].") ".date("H:i:s",$time);
-	return $date_str;
+  if(preg_match("/^\d+$/",$time)){
+    $time = intval($time);
+  }else{
+    $time = strtotime($time);
+  }
+  $youbi = ['日','月','火','水','木','金','土'];
+  $date_str = date("Y/m/d",$time)."(".$youbi[intval(date("w",$time))].") ".date("H:i:s",$time);
+  return $date_str;
 }
 
 ?>
@@ -43,26 +45,26 @@ function time2str($time){
 <meta charset="utf-8">
 <style>
 body {
-	background-color:#efefef;
+  background-color:#efefef;
 }
 h1 {
-	color:red;
-	font-size:larger;
-	margin:-.5em 0 0;
+  color:red;
+  font-size:larger;
+  margin:-.5em 0 0;
 }
 #main {
-	width:100%;
+  width:100%;
   max-width:800px;
 }
 dd {
-	margin-bottom:20px;
+  margin-bottom:20px;
 }
 span.id {
-	text-decoration:underline;
-	cursor:pointer;
+  text-decoration:underline;
+  cursor:pointer;
 }
 span.id:hover {
-	color:#888;
+  color:#888;
 }
 
 .green {
@@ -73,19 +75,18 @@ span.id:hover {
   font-weight:bold;
 }
 </style>
-<script src="js/jquery-2.1.3.min.js"></script>
-<script src="js/script.js"></script>
-<?php if($ERROR):?>
+
+<?php if(count($error)):?>
 <meta name="robots" content="noindex">
-<title><?=$ERROR_TITLE?></title>
+<title><?=$error['title']?></title>
 <?php else:?>
 <title><?=$info->thumb->title?>のコメント</title>
 <?php endif;?>
 </head>
 <body>
 
-<?php if($ERROR):?>
-<span class="msg"><?=$MSG?></span>
+<?php if(count($error)):?>
+<span class="msg"><?=$error['message']?></span>
 <a href="./">トップへ戻る</a>
 
 <?php else:?>
@@ -94,49 +95,26 @@ span.id:hover {
 <div id="main">
 <h1><?=$info->thumb->title?></h1>
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2218764024745094"
-     data-ad-slot="7115113567"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<?php include '_ads.html'; ?>
 
 <?php include '_form.html'; ?>
 
 <dl>
-	<dt>0：<font color=green>うｐ主</font>：<?=time2str($info->thumb->first_retrieve)?> ID:<?=$info->thumb->user_id?><dd><?=$info->thumb->description?><br><a href="http://www.nicovideo.jp/watch/<?=$v?>"><img src="<?=$info->thumb->thumbnail_url?>"></a></dd></dt>
+  <dt>0：<font color=green><?=$info->thumb->user_nickname?></font>：<?=time2str($info->thumb->first_retrieve)?> ID:<?=$info->thumb->user_id?><dd><?=$info->thumb->description?><br><a href="http://www.nicovideo.jp/watch/<?=$v?>"><img src="<?=$info->thumb->thumbnail_url?>"></a></dd></dt>
 
-	<?php foreach($comment_data as $c):?>
-	<dt><?=$c->vpos_time?>：<font color=green>名無し</font>：<?=time2str($c->date)?> ID:<span class="id <?=$c->user_id?>"><?=$c->user_id?></span><dd><?=$c->msg?></dd></dt>
-	<?php endforeach;?>
+  <?php foreach($comment_data as $c):?>
+  <dt><?=$c->vpos_time?>：<font color=green>名無し</font>：<?=time2str($c->date)?> ID:<span class="id <?=$c->user_id?>"><?=$c->user_id?></span><dd><?=$c->msg?></dd></dt>
+  <?php endforeach;?>
 </dl>
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2218764024745094"
-     data-ad-slot="7115113567"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<?php include '_ads.html'; ?>
 
 </div>
 
 <?php endif;?>
+<?php include '_analytics.html'; ?>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-64956232-2', 'auto');
-  ga('send', 'pageview');
-
-</script>
+<script src="js/jquery-2.1.3.min.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
