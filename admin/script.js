87006aaebceb8132abc4bc6fbf50321b0a67b020
Daiki Matoba <telnetstat@gmail.com>
2016-07-15 22:38:13 +0900

    Update css

--- admin/script.js
+++ admin/script.js
@@ -1,36 +1,40 @@
+var loading_on = function() {$('#loading').removeClass('hidden')};
+var loading_off = function() {$('#loading').addClass('hidden')};
 
 $('.login-form').on('submit', function(e) {
   e.preventDefault();
   var $form = $(this);
   var $input = $('[name=pass]', $form);
   if ($input.val() == '')return false;
+
+  loading_on();
   var r = $.post('/admin/api.php?login', {pass: $input.val()});
-  $('#loading').addClass('show');
+  r.always(loading_off);
   r.done(function() {
-    $('#loading').removeClass('show');
     $form.remove()
     fetch_contacts();
   });
   r.fail(function() {
-    $('#loading').removeClass('show');
     $input.val('');
     alert('Failed to login!');
   });
 });
 
 var fetch_contacts = function() {
+  var $contacts = $('.contacts').addClass('hidden');
+
+  loading_on();
   var r = $.get('/admin/api.php?contacts');
+  r.always(loading_off);
   r.done(function(json) {
-    $('#loading').removeClass('show');
     var tmpl = _.template('<tr><td><%- date %></td><td><%- comment %></td><td><%- ip_addr %></td></tr>');
     var tbody = json.map(function(contact) {
       return tmpl(contact).replace(/&lt;br&gt;/g, '<br>');
     }).join('');
-    $('.contacts tbody').html(tbody);
-    $('.contacts').addClass('loaded');
+    $('tbody', $contacts).html(tbody);
+    $contacts.fadeIn().removeClass('hidden');
   });
   r.fail(function() {
-    $('#loading').removeClass('show');
     alert('Failed to load');
   });
-}
+};
